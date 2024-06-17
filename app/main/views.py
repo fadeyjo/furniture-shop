2ca03f96f0cb12dce291fa1c4a1d2ac1f3843d67
fadeyjo <komrad.gubi2017@yandex.ru>
2024-06-17 16:33:49 +0300

    Tha main page is ready

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Main page'
+        'title': 'Home - главная',
+        'content': 'Магазин мебели HOME'
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Home - о нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о том, почему этот магазин такой классный, и какой хорший товар.'
+    }
+    return render(request, 'main/about.html', context)
